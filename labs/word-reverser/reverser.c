5f50fc9064c73301f34e3aef8497f310d9620d4e
ggonzaleze <ginaglez91@gmail.com>
2020-03-02 22:35:33 -0600

    Solve word-reverser lab
    Signed-off-by: ggonzaleze <ginaglez91@gmail.com>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(){
-    // Place your magic here
-    return 0;
+	// Place your magic here
+	char c;
+	char reversed[500];
+	char tmp[500];
+	int notEOF = 1;
+
+	while(notEOF){
+		strcpy(reversed,"");
+		while((c = getchar()) != '\n' && notEOF == 1){
+			if(c == EOF){
+				notEOF = 0;
+			}
+			else{
+				tmp[0] = c;
+				tmp[1] = '\0';
+				strcpy(reversed,strcat(tmp,reversed));
+			}
+		}
+		if (notEOF == 1)
+			printf("%s \n",reversed);
+	}
+	return 0;
 }

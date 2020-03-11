c01fb3de497d524a0d1d7478b4556c8069964f57
ggonzaleze <ginaglez91@gmail.com>
2020-03-10 20:41:06 -0600

    Solve maps and slices in Go lab
    Signed-off-by: ggonzaleze <ginaglez91@gmail.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,20 @@
 package main
 
-func main() {
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	counts := make(map[string]int)
+	words := strings.Split(s, " ")
+	for _,w := range words{
+		counts[w] = counts[w] + 1
+	}
+	return counts
 }
+
+func main() {
+	wc.Test(WordCount)
+}
+
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,18 @@
 package main
 
+import ("golang.org/x/tour/pic")
+
+func Pic(dx, dy int) [][]uint8 {
+	picture := make([][]uint8,dy,dy)
+	for i,_ := range picture{
+		picture[i] = make([]uint8,dx,dx)
+		for j,_ := range picture[i]{
+			picture[i][j] = uint8(i) + uint8(j)
+		}
+	}
+	return picture
+}
+
 func main() {
+	pic.Show(Pic)
 }

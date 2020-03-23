d591ec4affdd6a02b6242f542d4c31c3fe40614f
ggonzaleze <ginaglez91@gmail.com>
2020-03-22 21:37:02 -0600

    Solve str-lib lab
    Signed-off-by: ggonzaleze <ginaglez91@gmail.com>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    return 0;
+int mystrlen(char *);
+char *mystradd(char *, char *);
+int mystrfind(char *, char *);
+
+int main(int argc, char *args[]) {
+	if(argc < 4){
+		printf("Too few arguments \n");
+	}
+	else{
+		printf("Initial length      : %d\n",mystrlen(args[1]));
+		char *newStr = mystradd(args[1],args[2]);
+		printf("New String          : %s\n",newStr);
+		if(mystrfind(newStr,args[3]) == 1){
+			printf("SubString was found : yes\n");
+		}else{
+			printf("SubString was found : no\n");
+		}
+		free(newStr);
+	}
+	return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,57 @@
-
+#include <stdlib.h>
+#include <stdio.h>
 
 int mystrlen(char *str){
-    return 0;
+	int size = 0;
+	int i = 0;
+	while(str[i] != '\0'){
+		size++;
+		i++;
+	}
+	return size;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+	int sOrigin = mystrlen(origin);
+	int sAdd = mystrlen(addition);
+	int size = sOrigin + sAdd;
+	char *newStr = malloc(size + 1);
+	int i = 0;
+	int strI = 0;
+
+	while(origin[i] != '\0'){
+		newStr[strI] = origin[i];
+		strI++;
+		i++;
+	}
+	i = 0;
+	while(addition[i] != '\0'){
+		newStr[strI] = addition[i];
+		strI++;
+		i++;
+	}
+	newStr[strI] = '\0';
+	return newStr;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+	int sOrigin = mystrlen(origin);
+	int sSubs = mystrlen(substr);
+	int iO = 0;
+	int iS = 0;
+	while(iO < sOrigin){
+		if(origin[iO] == substr[iS]){
+			while(origin[iO] == substr[iS] && iS < sSubs){
+				iO++;
+				iS++;
+			}
+			if(substr[iS] == '\0'){return 1;}
+			else{
+				iS = 0;
+				continue;
+			}
+		}
+		iO++;
+	}
+	return 0;
 }
